64c99f34eb9e8613582762e41e786f92eb3b7586
ZainW <zainthegreatcb@gmail.com>
2017-03-13 12:39:14 -0400

    fix some db issues with the hero plural issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310163205) do
+ActiveRecord::Schema.define(version: 20170310182236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20170310163205) do
     t.datetime "updated_at",   null: false
   end
 
-  create_table "heros", force: :cascade do |t|
+  create_table "heroes", force: :cascade do |t|
     t.string   "name"
     t.string   "profile_url"
     t.integer  "base_str"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170310163205) do
     t.string   "attack_type"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.jsonb    "talents"
   end
 
 end
